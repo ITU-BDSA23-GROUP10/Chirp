6e59c4b226e609c14995fc3982763cbef672e1fc
August Brandt <aubr@itu.dk>
2023-09-12 12:08:59 +0200

    Refactor writing to CSV file to use CsvHelper
    Co-authored-by: jonasSkjodt <skjo@itu.dk>
    
    Co-authored-by: Nayla <nayh@itu.dk>
    
    Co-authored by: Theis <thph@itu.dk>
    
    Co-authored-by: Alex <lolk@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.VisualBasic.FileIO;
-using CsvHelper;
+﻿using CsvHelper;
 using System.Globalization;
-using CsvHelper.Expressions;
+using CsvHelper.Configuration;
 
 if (args.Length == 0)
 {
@@ -15,69 +14,54 @@ if (args.Length == 0)
 if (args[0] == "read")
 {
     // Read datafile with CsvHelper
-    IEnumerable<Cheep> records;
+    // https://joshclose.github.io/CsvHelper/examples/writing/appending-to-an-existing-file/
     using (var reader = new StreamReader(@"data/chirp_cli_db.csv"))
     using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
     {
-        records = csv.GetRecords<Cheep>();
-            // Show cheeps in console
+        var records = csv.GetRecords<Cheep>();
+        // Show cheeps in console
         foreach (var Cheep in records)
         {
-            Console.Write($"{Cheep.Author} @ {Cheep.Message}: {Cheep.Timestamp}\n");
+            // Convert from unix timestamp to DateTime for local time
+            // Code adapted from Stackoverflow answer: https://stackoverflow.com/a/250400
+            DateTime Cheeptime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            Cheeptime = Cheeptime.AddSeconds(Cheep.Timestamp).ToLocalTime();
+            
+            Console.Write($"{Cheep.Author} @ {Cheeptime}: {Cheep.Message}\n");
         }
     }
-
-
-    /*
-    // Reading chirp csv file to store them in a list. 
-    // Adapted form Stackoverflow answer: https://stackoverflow.com/a/33796861
-    var Cheeps = new List<(string, string, DateTime)>();
-    using (TextFieldParser csvParser = new TextFieldParser(@"data/chirp_cli_db.csv"))
-    {
-        csvParser.CommentTokens = new string[] { "#" };
-        csvParser.SetDelimiters(new string[] { "," });
-        csvParser.HasFieldsEnclosedInQuotes = true;
-
-        // Skip the row with the column names
-        csvParser.ReadLine();
-
-        while (!csvParser.EndOfData)
-        {
-            // Read current line fields, pointer moves to the next line.
-            string[] fields = csvParser.ReadFields();
-            if (fields != null)
-            {
-                string Name = fields[0];
-                string Message = fields[1];
-                // Convert from unix timestamp to DateTime for local time
-                // Code adapted from Stackoverflow answer: https://stackoverflow.com/a/250400
-                DateTime Cheeptime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-                Cheeptime = Cheeptime.AddSeconds(int.Parse(fields[2])).ToLocalTime();
-                
-                Cheeps.Add((Name, Message, Cheeptime));
-            }
-        }
-    }
-    */
 }
 else if (args[0] == "cheep")
 {
     var Message = args[1];
 
-    // Opens CSV data file in 'Append mode'
-    // code taken from https://learn.microsoft.com/en-us/dotnet/api/system.io.file.appendtext?view=net-7.0
-    using (StreamWriter Writer = new StreamWriter(@"data/chirp_cli_db.csv", append: true))
+    // New write with CsvHelper
+    var newCheep = new Cheep
     {
-        // Get system name
-        var Name = Environment.UserName;
-        // Stackoverflow answer: https://stackoverflow.com/a/35425123
-        var Time = DateTimeOffset.UtcNow.ToUnixTimeSeconds(); 
-        Writer.WriteLine($"{Name}, \"{Message}\", {Time}");
+        Message = Message,
+        Author = Environment.UserName,
+        Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+    };
+    var records = new List<Cheep> {newCheep};
+    
+    // Write to new cheep to csv file using CsvHelper
+    // https://joshclose.github.io/CsvHelper/examples/writing/appending-to-an-existing-file/
+    var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+    {
+        // Don't write the header again.
+        HasHeaderRecord = false,
+    };
+
+    using (var stream = File.Open("data/chirp_cli_db.csv", FileMode.Append))
+    using (var writer = new StreamWriter(stream))
+    using (var csv = new CsvWriter(writer, config))
+    {
+        csv.WriteRecords(records);
     }
 }
 public class Cheep
 {
     public string Author { get; set; }
     public string Message { get; set; }
-    public int Timestamp { get; set; }
+    public long Timestamp { get; set; }
 }
