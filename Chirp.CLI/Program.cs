760ab46077b3b109d2793d3f943d6ff399194499
JonasSkjodt1 <skjo@itu.dk>
2023-09-13 08:14:45 +0200

    Refactored program.cs to integrate docopt

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,96 +1,61 @@
-﻿using SimpleDB;
-using DocoptNet;
+﻿using DocoptNet;
+using SimpleDB;
+using System;
+using System.Collections.Generic;
 
 
-    IDatabaseRepository<Cheep> db = new CSVDatabase<Cheep>();  
-
-    UserInterface ui = new UserInterface();
-// This is taken from this website
-// https://docopt.github.io/docopt.net/dev/#api
-    const string help = @"Chirp
-
-Usage:
-  --read
-  --cheep <argument>
-  -help
-
-Options:
-  -help                 This shows the help screen.
-  --read                This prints all the cheeps.
-  --cheep <message>     This will post a cheep.
- 
-";
-static int Run(IDictionary<string, ArgValue> arguments)
+class Program
 {
-    foreach (var (key, value) in arguments)
-        Console.WriteLine("{0} = {1}", key, value);
-    return 0;
-}
+    private const string Help =
+            @"Chirp
 
-static int OnError(string usage) { Console.Error.WriteLine(usage); return 1; }
-static int ShowHelp(string help) { Console.WriteLine(help); return 0; }
-// var parser = Docopt.CreateParser(help)
-//              //.WithVersion("")
-//              .Parse(args)
-//              .Match(Run,
-//                     result => ShowHelp(result.Help),
-//                     result => OnError(result.Usage));
-var parser = Docopt.CreateParser(help);
-//This is the correct way to do it but apparently args is not the right type...
-var result = parser.Parse(args);
+            Usage:
+            Chirp (--read | --cheep <message>)
+            Chirp -h | --help
 
-IDictionary<string, ArgValue> dictionary = result;
-return 0;
+            Options:
+            -h --help         Show this screen.
+            --read            Print all the cheeps.
+            --cheep <message> Post a cheep.
 
+            To get this help screen: 
+            dotnet run -- -h
+            ";
 
+    private static IDatabaseRepository<Cheep> db = new CSVDatabase<Cheep>();
+    private static UserInterface ui = new UserInterface();
 
-
-
-// if (args.Length == 0)
-// {
-//     Console.WriteLine("Welcome to Chirp. Chirp is a the platform formerly known as twitter clone developed by 5 idiots at ITU. Enjoy!\n");
-//     Console.WriteLine("Usage:\n\tTo read cheeps (tweeds) type 'dotnet run --read'\n" +
-//                         "\tTo right a cheep type 'dotnet run -- cheep <message>'");
-//     return;
-// }
-static int readCheeps(IDatabaseRepository<Cheep> db, UserInterface ui) 
-{
-    var cheeps = db.Read();
-    ui.PrintCheeps(cheeps); 
-    return 0;
-}
-
-static int postCheep(IDatabaseRepository<Cheep> db, string Message) 
-{
-    //New write with CsvHelper
-    var newCheep = new Cheep
+    static void Main(string[] args)
     {
-        Message = Message,
-        Author = Environment.UserName,
-        Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
-    };
-    var records = new List<Cheep> {newCheep};
-    db.Store(records);
-    return 0;
+        // Docopt code documentation reworked from https://csharp.hotexamples.com/site/file?hash=0xff1fe91471d0685c57d186e7951993e3fbf382bccde2e3daaaa6c22982498b4b
+        // and https://docopt.github.io/docopt.net/dev/#api
+        var arguments = new Docopt().Apply(Help, args, exit: true);
+
+        if (arguments["--read"].IsTrue)
+        {
+            ReadCheeps();
+        }
+        else if (arguments["--cheep"].IsTrue)
+        {
+            PostCheep(arguments["<message>"].Value.ToString());
+        }
+    }
+
+    static void ReadCheeps() 
+    {
+        var cheeps = db.Read();
+        ui.PrintCheeps(cheeps); 
+    }
+
+    static void PostCheep(string message) 
+    {
+        var newCheep = new Cheep
+        {
+            Message = message,
+            Author = Environment.UserName,
+            Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+        };
+        var records = new List<Cheep> {newCheep};
+        db.Store(records);
+    }
 }
-
-// if (args[0] == "read")
-// {
-//     var cheeps = db.Read();
-//     ui.PrintCheeps(cheeps);
-    
-// }
-// else if (args[0] == "cheep")
-// {
-//     var Message = args[1];
-
-//     // New write with CsvHelper
-//     var newCheep = new Cheep
-//     {
-//         Message = Message,
-//         Author = Environment.UserName,
-//         Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
-//     };
-//     var records = new List<Cheep> {newCheep};
-//     db.Store(records);
-// }
