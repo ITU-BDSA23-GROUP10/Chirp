9d2e928180bd575062c740d68248f79fafa5d917
August Brandt <aubr@itu.dk>
2023-09-19 11:36:28 +0200

    Refactor database interface to adhere to course instructions

--- src/SimpleDB/IDatabaseRepository.cs
+++ src/SimpleDB/IDatabaseRepository.cs
@@ -3,5 +3,5 @@ namespace SimpleDB;
 public interface IDatabaseRepository<T>
 {
     public IEnumerable<T> Read(int? limit = null);
-    public void Store(List<T> records);
+    public void Store(T record);
 }
