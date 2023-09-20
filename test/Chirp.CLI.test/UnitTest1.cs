cb7060b3bb001fb6a71c8c5c752c8d3361382ce6
Alexander Lolk <Lolk@itu.dk>
2023-09-20 11:56:50 +0200

    Made inprovement to endtoend test and readded the tests back in
    Co-authored-by: August <aubr@itu.dk>
    
    Co-authored-by: jonasSkjodt <skjo@itu.dk>
    
    Co-authored-by: Theis <thph@itu.dk>

---
+++ test/Chirp.CLI.test/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace Chirp.CLI.test;
+
+public class UnitTest1
+{
+
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/SimpleDB.test/SimpleDB.test.cs
@@ -0,0 +1,44 @@
+using SimpleDB;
+using Xunit;
+using System.Collections.Generic;
+
+// basic tests
+namespace SimpleDB.Tests
+{
+    public class CSVDatabaseTests
+    {
+        /*
+        [Fact]
+        public void ReadMethod_ShouldReadData()
+        {
+            // Arrange
+            var csvdb = new CSVDatabase<Cheep>();
+
+            // Act
+            var records = csvdb.Read();
+
+            // Assert
+            Assert.NotEmpty(records);
+        }
+
+        [Fact]
+        public void StoreMethod_ShouldStoreData()
+        {
+            // Arrange
+            var csvdb = new CSVDatabase<Cheep>();
+            var cheepList = new List<Cheep>
+            {
+                //placeholder
+                new Cheep { Author = "karpe", Message = "temp", Timestamp = 1694524141 }
+            };
+
+            // Act
+            csvdb.Store(cheepList);
+
+            // Assert
+            var records = csvdb.Read();
+            Assert.Contains(records, record => record.Author == "karpe" && record.Message == "temp");
+        }
+        */
+    }
+}
