3fdbbdeffac38308cb99ce8e87c1a8c59b4c90b5
Nayla <nayh@itu.dk>
2023-09-21 16:36:43 +0200

    Test (passed) to ensure singleton-pattern db.

--- test/SimpleDB.test/SimpleDB.test.cs
+++ test/SimpleDB.test/SimpleDB.test.cs
@@ -38,5 +38,29 @@ namespace SimpleDB.Tests
             // var records = csvdb.Read();
             // Assert.Contains(records, record => record.Author == "karpe" && record.Message == "temp");
         }
+
+        [Fact]
+        public void CSVDbSingleton_ShouldOnlyCreateOne()
+        {
+            // Arrange
+            CSVDbSingleton dbSingleton1 = CSVDbSingleton.Instance;
+            CSVDbSingleton dbSingleton2 = CSVDbSingleton.Instance;
+
+            IDatabaseRepository<Cheep> db1 = dbSingleton1.Database;
+            IDatabaseRepository<Cheep> db2 = dbSingleton2.Database;
+
+            Cheep newCheep = new Cheep
+            {
+                Message = "Testing Singleton",
+                Author = Environment.UserName,
+                Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+            };
+
+            // Act
+            db2.Store(newCheep);
+
+            // Assert
+            Assert.Equal(db1, db2);
+        }
     }
 }
