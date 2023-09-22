acc6489c5684a22ac5533fdb181490066ccb96b6
Theis <thph@itu.dk>
2023-09-22 15:05:29 +0200

    Tried to fix the issues with localhost not showing things on the webinterface in Chirp.CSVDBService

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -3,12 +3,33 @@ using Chirp;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
+SingletonDB dbSingleton = SingletonDB.Instance;
+IDatabaseRepository<SimpleDB.Cheep> db = dbSingleton.Database;
 
-app.MapPost("/cheep", () => new Cheep("me", "Hej!", 1684229348));
+
+
+var newCheep = new SimpleDB.Cheep
+        {
+            Message = "test",
+            Author = "hejehj",
+            Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+        };
+
+//app.MapPost("/cheep", () => new Chirp.Program.PostCheep(message));
 //ReadCheeps
-app.MapGet("/cheeps", () => Chirp.Program.ReadCheeps());
+//app.MapGet("/cheeps", () => {});
+
+//This shows all the cheeps from the db onto the localhost as a JSON file
+app.MapGet("/cheeps", () => db.Read());
+
+//This works to create a new cheep in the CSVfile but it uses Get so it clearly is wrong
+//app.MapGet("/cheep", () => db.Store(newCheep));
+
+//This resuls in a 405 method not allowed on the network tab when you try it in a webbrowser and doesnt actually save the cheep
+app.MapPost("/cheep", (Cheep cheep) => db.Store(cheep));
 
 app.Run();
+
+
 
 public record Cheep(string Author, string Message, long Timestamp);
