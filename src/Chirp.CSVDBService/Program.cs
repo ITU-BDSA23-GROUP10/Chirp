d588bc242e2ff12d1813d3d503614a7b63937e8e
JonasSkjodt1 <skjo@itu.dk>
2023-09-25 13:36:55 +0200

    fixed the mappost method in csvdbservice
    Co-authored-by: Theis <thph@itu.dk>
    
    Co-authored-by: Alex <lolk@itu.dk>

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -6,8 +6,6 @@ var app = builder.Build();
 SingletonDB dbSingleton = SingletonDB.Instance;
 IDatabaseRepository<SimpleDB.Cheep> db = dbSingleton.Database;
 
-
-
 var newCheep = new SimpleDB.Cheep
         {
             Message = "test",
@@ -26,10 +24,24 @@ app.MapGet("/cheeps", () => db.Read());
 //app.MapGet("/cheep", () => db.Store(newCheep));
 
 //This resuls in a 405 method not allowed on the network tab when you try it in a webbrowser and doesnt actually save the cheep
-app.MapPost("/cheep", (Cheep cheep) => db.Store(cheep));
-
+//app.MapPost("/cheep", (Cheep cheep) => db.Store(cheep));
+app.MapPost("/cheep", (Cheep newCheep) => 
+{
+    var cheepWithTimestamp = new SimpleDB.Cheep 
+    { 
+        Author = newCheep.Author, 
+        Message = newCheep.Message, 
+        Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds() 
+    };
+    db.Store(cheepWithTimestamp);
+    return Results.Created($"/cheep/{cheepWithTimestamp.Timestamp}", cheepWithTimestamp);
+});
 app.Run();
 
+/*
+to see the post through curl (make sure your csv data file has the actual data in it)
+// curl -X POST -H "Content-Type: application/json" -d "{\"Author\":\"Alexa\", \"Message\":\"measse\"}" http://localhost:5076/cheep
+*/
 
 
 public record Cheep(string Author, string Message, long Timestamp);
