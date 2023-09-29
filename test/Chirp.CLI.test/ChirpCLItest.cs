87f28e9ecfd8dcef778de0adac2438b02e0d2126
JonasSkjodt1 <skjo@itu.dk>
2023-09-29 09:56:49 +0200

    integration tests for chirpCLI inserted
    Co-authored-by: Alex <lolk@itu.dk>
    Co-authored-by: August <aubr@itu.dk>

--- test/Chirp.CLI.test/ChirpCLItest.cs
+++ test/Chirp.CLI.test/ChirpCLItest.cs
@@ -1,10 +1,33 @@
-namespace test;
+using Xunit;
+using System.Net.Http;
+using System.Threading.Tasks;
 
-public class ChirpCLItest
+namespace ChirpCLI.Tests
 {
-    /*[Fact]
-    public void Test1()
+    public class ChirpCLITests
     {
+        private readonly HttpClient client;
+        
+        public ChirpCLITests()
+        {
+            client = new HttpClient
+            {
+                BaseAddress = new Uri("https://bdsagroup10chirpremotedb.azurewebsites.net/")
+            };
+        }
 
-    }*/
+        // Integration tests
+        [Fact]
+        public async Task HttpClient_IsCorrectlyConfigured()
+        {
+            //arrange
+            var expectedBaseAddress = new Uri("https://bdsagroup10chirpremotedb.azurewebsites.net");
+            
+            //act
+            var actualBaseAdress = client.BaseAddress;
+            
+            //assert
+            Assert.Equal(expectedBaseAddress, actualBaseAdress);
+        }
+    }
 }
