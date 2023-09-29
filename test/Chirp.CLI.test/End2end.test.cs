ca6cb105da5793782a46c856063cac9d6030f323
Alexander Lolk <Lolk@itu.dk>
2023-09-29 11:44:23 +0200

    Removed old endtoend test
    Co-authored-by: jonasSkjodt <skjo@itu.dk>
    
    Co-authored-by: August <aubr@itu.dk>

--- test/Chirp.CLI.test/End2end.test.cs
+++ test/Chirp.CLI.test/End2end.test.cs
@@ -97,6 +97,24 @@ public class endToEndTest
 
     }
 
+    /*[Fact]
+    public async void http_postCheep_And_Read_After_EndtoEnd_Test()
+    {
+        // Arrange
+        var requestGet = new HttpRequestMessage(HttpMethod.Get, "cheeps");
+        var requestPost = new HttpRequestMessage(HttpMethod.Post, "cheeps");
+
+        // Act
+        var response = await client.SendAsync(request);
+        var content = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.True(content.Contains("{\"author\":\"ropf\",\"message\":\"Hello, BDSA students!\",\"timestamp\":1690891760}"));
+        Assert.True(content.Contains("{\"author\":\"rnie\",\"message\":\"Welcome to the course!\",\"timestamp\":1690978778}"));
+        Assert.True(content.Contains("{\"author\":\"rnie\",\"message\":\"I hope you had a good summer.\",\"timestamp\":1690979858}"));
+        Assert.True(content.Contains("{\"author\":\"ropf\",\"message\":\"Cheeping cheeps on Chirp :)\",\"timestamp\":1690981487}"));
+    }*/
+
 
 
 }
