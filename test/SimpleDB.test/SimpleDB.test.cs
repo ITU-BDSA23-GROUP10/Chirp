f42afb48d60c0d29b7f583c4383738746132822b
JonasSkjodt1 <skjo@itu.dk>
2023-09-29 09:28:41 +0200

    simpleDB tests now working with azure server
    Co-authored-by: Alex <lolk@itu.dk>
    Co-authored-by: August <aubr@itu.dk>

--- test/SimpleDB.test/SimpleDB.test.cs
+++ test/SimpleDB.test/SimpleDB.test.cs
@@ -1,6 +1,4 @@
-using Xunit;
 using System.Net;
-using System.Net.Http;
 using Newtonsoft.Json;
 using System.Text;
 using SimpleDB;
@@ -15,12 +13,11 @@ namespace CSVDatabaseWebService.Tests
         {
             client = new HttpClient
             {
-                BaseAddress = new Uri("http://localhost:5076/")
+                BaseAddress = new Uri("https://bdsagroup10chirpremotedb.azurewebsites.net/")
             };
         }
 
         // Integration tests
-        /*
         [Fact]
         public async Task GetCheeps_ShouldReturn200()
         {
@@ -32,11 +29,9 @@ namespace CSVDatabaseWebService.Tests
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        }*/
+        }
 
-        /*
-        this is failing*/
-        /*[Fact]
+        [Fact]
         public async Task PostCheep_ShouldReturn200()
         {
             // Arrange
@@ -54,7 +49,7 @@ namespace CSVDatabaseWebService.Tests
             //Post sends a 201 success
             Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         }
-        */
+        
         [Fact]
         public void SingletonDB_ShouldOnlyCreateOne()
         {
@@ -79,72 +74,3 @@ namespace CSVDatabaseWebService.Tests
         
     }
 }
-
-/*using SimpleDB;
-using Xunit;
-using System.Collections.Generic;
-
-// basic tests
-namespace SimpleDB.Tests
-{
-    public class CSVDatabaseTests
-    {
-        [Fact]
-        public void ReadMethod_ShouldReadData()
-        {
-            //Arrange
-            var csvdb = new CSVDatabase<Cheep>();
-
-            // Act
-            var records = csvdb.Read();
-
-             // Assert
-            Assert.NotEmpty(records);
-        }
-
-        [Fact]
-        public void StoreMethod_ShouldStoreData()
-        {
-            // Arrange
-            var csvdb = new CSVDatabase<Cheep>();
-            var cheepList = new List<Cheep>
-            {
-                //placeholder
-                new Cheep { Author = "karpe", Message = "temp", Timestamp = 1694524141 }
-            };
-
-            // Act
-            foreach(var cheep in cheepList) 
-            {
-                csvdb.Store(cheep);
-            }
-
-            // Assert
-            var records = csvdb.Read();
-            Assert.Contains(records, record => record.Author == "karpe" && record.Message == "temp");
-        }
-
-        [Fact]
-        public void SingletonDB_ShouldOnlyCreateOne()
-        {
-            // Arrange
-            SingletonDB dbSingleton1 = SingletonDB.Instance;
-            SingletonDB dbSingleton2 = SingletonDB.Instance;
-            IDatabaseRepository<Cheep> db1 = dbSingleton1.Database;
-            IDatabaseRepository<Cheep> db2 = dbSingleton2.Database;
-            Cheep newCheep = new Cheep
-            {
-                Message = "Testing Singleton",
-                Author = Environment.UserName,
-                Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
-            };
-
-            // Act
-            db2.Store(newCheep);
-
-            // Assert
-            Assert.Equal(db1, db2);
-        }
-        
-    }
-}*/
