48ca22da8a5852e11934f40105ebb7397283f8d1
Theis <thph@itu.dk>
2023-10-03 09:12:52 +0200

    Created the Razor page project and added DBFacade to SimpleDB so that we use SQlite3 instead of CSV files now to store and read out data. This is the first draft of refactoring. We still need to add posting + more
    Co-authored-by: Alex <lolk@itu.dk>
    Co-authored-by: August <aubr@itu.dk>
    Co-authored-by: Jonas <skjo@itu.dk>
    Co-authored-by: Nayla <nayh@itu.dk>

---
+++ src/SimpleDB/DBFacade.cs
@@ -0,0 +1,42 @@
+using Microsoft.Data.Sqlite;
+
+namespace SimpleDB;
+
+
+public class DBFacade 
+{
+    private string sqlDBFilePath = "/tmp/chirp.db";
+
+    private string sqlQuery;
+
+
+    public List<Cheep> GetCheeps() 
+    {
+        sqlQuery = @"SELECT U.username, M.text, M.pub_date FROM message M JOIN user U ON U.user_id = M.author_id ORDER by M.pub_date desc";
+        List<Cheep> cheeps; 
+
+        using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+        {
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = sqlQuery;
+
+            using var reader = command.ExecuteReader();
+            cheeps = new List<Cheep>();
+            while (reader.Read())
+            { 
+              cheeps.Add(new Cheep() 
+              {
+                Author = reader.GetString(0), 
+                Message = reader.GetString(1), 
+                Timestamp = reader.GetInt64(2)
+              });  
+            }
+            return cheeps;
+        }
+    }
+   
+
+}
+
