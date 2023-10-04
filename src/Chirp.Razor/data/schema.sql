6bf06b3666af1a68f9094b111702bceb6cb7f3e1
Theis <thph@itu.dk>
2023-10-04 13:44:14 +0200

    Added the correct dump and schema files from the project repo

--- src/Chirp.Razor/data/schema.sql
+++ src/Chirp.Razor/data/schema.sql
@@ -2,7 +2,8 @@ drop table if exists user;
 create table user (
   user_id integer primary key autoincrement,
   username string not null,
-  email string not null
+  email string not null,
+  pw_hash string not null
 );
 
 drop table if exists message;
