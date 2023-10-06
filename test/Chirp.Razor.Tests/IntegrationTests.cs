45f0d06981c99d91733a0bbffc7c272dfc1fde73
JonasSkjodt1 <skjo@itu.dk>
2023-10-06 12:21:04 +0200

    added basic integration test
    Co-authored-by: Alex <lolk@itu.dk>

--- test/Chirp.Razor.Tests/IntegrationTests.cs
+++ test/Chirp.Razor.Tests/IntegrationTests.cs
@@ -1,6 +1,38 @@
 namespace Chirp.Razor.Tests;
 using Microsoft.AspNetCore.Mvc.Testing;
 
+public class BasicTests 
+    : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public BasicTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Theory]
+    [InlineData("/")]
+    [InlineData("/Index")]
+    [InlineData("/About")]
+    [InlineData("/Privacy")]
+    [InlineData("/Contact")]
+    public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync(url);
+
+        // Assert
+        response.EnsureSuccessStatusCode(); // Status Code 200-299
+        Assert.Equal("text/html; charset=utf-8", 
+            response.Content.Headers.ContentType.ToString());
+    }
+}
+
+/*
 public class IntegrationTest : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _fixture;
@@ -39,4 +71,4 @@ public class IntegrationTest : IClassFixture<WebApplicationFactory<Program>>
     }
 }
 
-public partial class Program { }
+public partial class Program { }*/
