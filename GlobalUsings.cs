fea56bbd80bd9c079164c1add0a9187c2cce509b
Nayla <nayh@itu.dk>
2023-10-10 10:57:27 +0200

    Added GlobalUsings in relevant .csproj

---
+++ GlobalUsings.cs
@@ -0,0 +1,7 @@
+// Adding this file to .csproj from:
+//https://ingebrigtsen.blog/2021/08/13/c-10-reuse-global-usings-in-multiple-projects/
+
+// Namespaces
+using SimpleDB;
+
+// 3rd-party class libraries
