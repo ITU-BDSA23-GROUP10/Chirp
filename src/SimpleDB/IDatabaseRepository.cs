806972b5d628b578c9b0557d48506d238f5a5baf
Nayla <nayh@itu.dk>
2023-10-10 11:07:20 +0200

    Refactored IDBRepo<T> to fit Remondo's (ref) repo pattern.

--- src/SimpleDB/IDatabaseRepository.cs
+++ src/SimpleDB/IDatabaseRepository.cs
@@ -1,7 +1,17 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
 namespace SimpleDB;
+
+// Repository pattern from Remondo:
+// https://web.archive.org/web/20150404154203/https://www.remondo.net/repository-pattern-example-csharp/
 
 public interface IDatabaseRepository<T>
 {
-    public IEnumerable<T> Read(int? limit = null);
-    public void Store(T record);
+    void Insert(T entity);
+        void Delete(T entity);
+        IQueryable<T> SearchFor(Expression<Func<T, bool>> predicate);
+        IQueryable<T> GetAll();
+        T GetById(int id);
 }
