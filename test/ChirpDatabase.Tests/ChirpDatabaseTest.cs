abe04269c2820506c7ca8ba95bca2b199fee9092
August Brandt <aubr@itu.dk>
2023-11-12 22:06:34 +0100

    Creates the first working test with testcontainers

--- test/ChirpDatabase.Tests/ChirpDatabaseTest.cs
+++ test/ChirpDatabase.Tests/ChirpDatabaseTest.cs
@@ -1,5 +1,9 @@
 using Testcontainers.MsSql;
 using Bogus;
+using Chirp.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Chirp.Infrastructure.ChirpRepository;
+using Chirp.Core;
 
 namespace ChirpIntegraiton.Tests;
 
@@ -19,8 +23,29 @@ public class ChirpDatabaseTest : IAsyncLifetime
     }
 
     [Fact]
-    public void Test1()
-    {
-        Assert.True(true);
+    public async void CreateACheepInDatabase_WhereAuthorExists()
+    {   
+        // Assert
+        var contextOptions = new DbContextOptionsBuilder<ChirpDBContext>()
+            .UseSqlServer(_sqlServer.GetConnectionString())
+            .Options;
+        var context = new ChirpDBContext(contextOptions);
+        context.Database.Migrate();
+
+        var cheepService = new CheepRepository(context);
+        var authorService = new AuthorRepository(context);
+
+        // Act
+        var authorName = "Test author";
+        await authorService.CreateAuthor(authorName);
+        var cheep = new CheepCreateDTO("Test message", authorName);
+
+        await cheepService.CreateCheep(cheep, await authorService.GetAuthorByName(authorName));
+        
+        var dbCheep = cheepService.GetAll();
+
+        // Assert
+        Assert.Equal(1, dbCheep.Item2);
+        Assert.Equal("Test message", dbCheep.Item1.FirstOrDefault().Text);
     }
 }
