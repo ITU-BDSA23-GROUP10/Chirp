cbf7e8357acea0480904458774b70e47c7a86001
JonasSkjodt1 <skjo@itu.dk>
2023-11-17 14:32:17 +0100

    inserted yml file for github ui testing
    Co-authored-by: Alex <lolk@itu.dk>

--- test/PlaywrightTests/UITesting.cs
+++ test/PlaywrightTests/UITesting.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 namespace PlaywrightTests;
 
 
-//run thes test
+//run these test
+//remember in order to use the pwsh command you need to update powershell (with net8.0) with: dotnet tool update --global PowerShell
 //pwsh bin/Debug/net8.0/playwright.ps1 codegen https://localhost:5273 --ignore-https-errors
 
 [Parallelizable(ParallelScope.Self)]
