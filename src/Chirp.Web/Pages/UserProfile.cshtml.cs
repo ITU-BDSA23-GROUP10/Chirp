8eb5015de0f64486e804844a253f5fa3e7e03da0
Alexander Lolk <Lolk@itu.dk>
2023-11-24 14:11:36 +0100

    Made sign out with forget me button work

--- src/Chirp.Web/Pages/UserProfile.cshtml.cs
+++ src/Chirp.Web/Pages/UserProfile.cshtml.cs
@@ -2,8 +2,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc;
 using Chirp.Core;
 using Chirp.Infrastructure.Models;
-
-
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace Chirp.Web.Pages;
 
@@ -11,39 +12,47 @@ public class UserProfileModel : PageModel
 {
     readonly IUserRepository<User> _userService;
 
-
 public UserProfileModel(IUserRepository<User> userService, IAuthorRepository<Author, Cheep, User> authorService, ICheepRepository<Cheep, Author> cheepService)
-    {
-        _userService = userService;
-    }
-    
+{
+    _userService = userService;
+}    
 
-    public async Task<ActionResult> OnGetAsync() {
+    public async Task<ActionResult> OnGetAsync()
+    {
         if(!User.Identity.IsAuthenticated) {
             return Redirect("/");
         }
+        
         if(await _userService.GetUserByName(User.Identity.Name) == null) {
             await _userService.CreateUser(User.Identity.Name);
         }
+
         var userName = User.Identity.Name;
         var user = await _userService.GetUserByName(userName);
+        
         ViewData["UserName"] = user.Name;
         ViewData["UserEmail"] = user.Email;
+
         return Page();
     }
 
-    public async Task<IActionResult> OnPostForgetMeAsync() {
+    public async Task<IActionResult> OnPostForgetMeAsync()
+    {
         if(!User.Identity.IsAuthenticated) {
             return Redirect("/");
         }
+
         var userName = User.Identity.Name;
         var user = await _userService.GetUserByName(userName);
         await _userService.DeleteAllFollowers(user.UserId);
         _userService.DeleteUser(user);
 
-
-
+        return await Logout();
+    }
+    // This was inspired by: https://learn.microsoft.com/en-us/entra/identity-platform/scenario-web-app-sign-user-sign-in?tabs=aspnetcore
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         return Redirect("/");
     }
-
 }
