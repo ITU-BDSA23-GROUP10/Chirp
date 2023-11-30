b44c16080ab471e458009e13566e48f781d59b33
Slug-Boi <thph@itu.dk>
2023-11-30 22:09:52 +0100

    removed a space

--- test/Chirp.Razor.Tests/IntegrationTests.cs
+++ test/Chirp.Razor.Tests/IntegrationTests.cs
@@ -225,5 +225,4 @@ public class IntegrationTest : IClassFixture<CustomWebApplicationFactory<Program
             Assert.Equal(message, retrievedAuthor.Cheeps[0].Text);
             }
     }
-    
 }
