3e9abac62451d2974e8eb36e25d673f4ec54d52b
Theis <thph@itu.dk>
2023-12-08 09:55:43 +0100

    Added a password file creation to bash script

--- create_cert.sh
+++ create_cert.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # This file was partly inspired by what Albert Rise Nielsen : https://github.com/albe2669 had shown me with their own script
 # This script is used to create a certificate for the server
+pss=$1
+
 mkdir ./.local
 
 cd ./.local
@@ -14,6 +16,21 @@ fi
 # Creates a .gitignore file
 touch .gitignore
 echo "*" >> .gitignore
+
+# Creates a password file
+if test -f ./cert_password.txt 
+then
+    rm ./cert_password.txt
+fi
+
+# Inspired by this: https://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
+touch ./cer_password.txt
+if [ -z "$pss+x" ]
+then
+    echo "$pss" >> ./cer_password.txt
+else
+    echo "YourGonnaBurnAlright123456" >> ./cer_password.txt
+fi
 
 # Removes the cert.pfx file if it exists
 if test -f ./cert.pfx 
