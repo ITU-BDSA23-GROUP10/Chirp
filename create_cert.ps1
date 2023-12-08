e8d68b69508e844097e99dceb088e13f49474a70
Theis <thph@itu.dk>
2023-12-08 12:52:12 +0100

    added an arg to powershell script

--- create_cert.ps1
+++ create_cert.ps1
@@ -1,9 +1,13 @@
 # This file was partly inspired by what Albert Rise Nielsen : https://github.com/albe2669 had shown me with their own script
 # This script is used to create a certificate for the server
 # The powershell version of this script was translated from the bash script by copilot chat <3
+$pss = $args[0]
+
 New-Item -ItemType Directory -Force -Path .\.local
 
 Set-Location .\.local
+
+
 
 # Removes the .gitignore file if it exists
 if (Test-Path .gitignore) 
